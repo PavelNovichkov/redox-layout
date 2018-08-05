e1cfe729f76ba9679f6001f0251d5d9463e7a057
Paul Novichkov <paul.novichkov@yandex.com>
2018-08-05 21:19:38 +0200

    Add base layer setup

---
+++ config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+// LEDs work only with serial communication and left master hand.
+// See https://github.com/mattdibi/redox-keyboard#rgb-underglow for details. 
+#define USE_SERIAL
+#define MASTER_LEFT
+
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 14
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+
+// My keyboard does not have mechanical lock switches.
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+#endif

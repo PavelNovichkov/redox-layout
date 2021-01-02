4ac8f245b0bcccacb419298c206aaa8bd4170ea5
Pavel Novichkov <paul.novichkov@yandex.com>
2021-01-02 16:50:17 +0100

    Setup tap-hold

--- config.h
+++ config.h
@@ -22,4 +22,10 @@
 // Double-tap sticky keys to (de)activate them permanently.
 #define ONESHOT_TAP_TOGGLE 2
 
+// Tap-hold.
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT // rollover without triggering mods
+#define TAPPING_FORCE_HOLD // no auto-repeat on double tap
+#define PERMISSIVE_HOLD
+
 #endif
